0de699888a711d774e73d55450a0506896352619
Juan <juanmachado123@hotmail.com>
2022-05-01 21:04:46 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var input1 = document.querySelector("#inp1")
+var input2 = document.querySelector("#inp2")
+var resultado = document.querySelector("#resultado")
+const btn = document.querySelector('#btnCalcular')
+
+btn.addEventListener("click", function(event){
+    event.preventDefault();
+    media1 = input1.value
+    media2 = input2.value
+    
+    var resu = MediaCalc(media1, media2)
+    resultado.textContent = `sua Média é: ${resu}`
+})
+
+function MediaCalc(valor, valo2){
+  return parseInt(valor) + parseInt(valo2) / 2
+}
+
+
